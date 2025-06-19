e4748cc5f01eb93bd115f8a2acdabfad15ac3e13
raouf-H8 <haroun.rraouf@gmail.com>
2025-06-19 22:18:45 +0000

    "ajout de .env et .env.render avec .gitignore"

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
 # contient la configuration technique de la BDD (engine, SessionLocal, DATABASE_URL)
-# 
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL") #le DATABASE_URL est défini dans le test.yml et docker-compose.yml
+#update pour utiliser le .env et charger les var d'env
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL") #le DATABASE_URL ETAIT ANCIENNEMENT défini dans le test.yml et docker-compose.yml 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
